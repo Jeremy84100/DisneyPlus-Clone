60521075bf2d8491113e5907c770d3fe79788f23
Jeremy84100 <96269524+Jeremy84100@users.noreply.github.com>
2023-05-19 21:33:55 +0200

    test bug

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -30,7 +30,7 @@ const MainLayout = () => {
   const [profileName, setProfileName] = useState<string>("");
   const [selectedImage, setSelectedImage] = useState<Image>({
     id: 16,
-    image: "public/images/profile/mickey-mouse/mickey-mouse.png",
+    image: "src/images/profile/mickey-mouse.png",
     alt: "Mickey Mouse",
     category: "Mickey Mouse",
   });
